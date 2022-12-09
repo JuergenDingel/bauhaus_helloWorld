349420553b7638d6fa6fe480e44213262f82b586
JuergenDingel <dingel@cs.queensu.ca>
2022-12-08 20:58:43 -0500

    added more run files

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 
 # These two lines make sure a faster SAT solver is used.
-from nnf import config
+from nnf import And, Or, Var, config
 config.sat_backend = "kissat"
 
 # Encoding that will store all of your constraints
@@ -25,13 +25,11 @@ class BasicPropositions:
 # that are instances of this class must be true by using a @constraint decorator.
 # other options include: at most one, exactly one, at most k, and implies all.
 # For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
+# @constraint.at_least_one(E)
 @proposition(E)
 class FancyPropositions:
-
     def __init__(self, data):
         self.data = data
-
     def __repr__(self):
         return f"A.{self.data}"
 
@@ -52,34 +50,49 @@ z = FancyPropositions("z")
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
-def example_theory():
+def example_encoding():
     # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
+    
+    # E.add_constraint((a | b) & ~x)
+    # E.add_constraint((a | b) & ~x)
     # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint((x & y).negate())
+    # E.add_constraint(y >> z)
+    # Negate a formula    
+#    E.add_constraint(~(x & y))
     # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
     # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
+#    constraint.add_exactly_one(E, a, b, c)
 
+    E.add_constraint((a | ~a))
     return E
-
+ 
 
 if __name__ == "__main__":
 
-    T = example_theory()
+    E = example_encoding()
     # Don't compile until you're finished adding all your constraints!
-    T = T.compile()
+    T = E.compile()
+    T2 = Or([T,Var('W')]).negate()
+    # E.introspect()
     # After compilation (and only after), you can check some of the properties
     # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
-
+    print("\nSatisfiable: %s" % T2.satisfiable())
+    print(" #solutions: %d" % count_solutions(T2))
+    soln = T2.solve()
+    # E.introspect(soln)    
+    print("   Pretty print of theory:")
+    E.pprint(T2, soln)
+    print("   Solution: %s" % soln)
     print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+    for v,vn in zip([a], 'a'):
         # Ensure that you only send these functions NNF formulas
         # Literals are compiled to NNF here
         print(" %s: %.2f" % (vn, likelihood(T, v)))
+#        print(" %s: %s" % (vn, v))
+    print("\nValid: %s" % T.valid())
+    # print("\nNegating theory:")
+    # T = T.negate()
+    # print("\nNegation satisfiable: %s" % T.satisfiable())
+    # soln = T.solve()    
+    # print("   Solution: %s" % soln)    
     print()
